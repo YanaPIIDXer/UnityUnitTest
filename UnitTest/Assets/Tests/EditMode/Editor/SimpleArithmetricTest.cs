b3c34c71cbd0690b7bea7292bfd70a96a4177e56
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-09 20:56:41 +0900

    テストを作成

---
+++ UnitTest/Assets/Tests/EditMode/Editor/SimpleArithmetricTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class SimpleArithmetricTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void AddTest()
+        {
+            SimpleArithmetric TestTarget = new SimpleArithmetric(1, 2);
+            Assert.AreEqual(TestTarget.Add(), 3);
+        }
+
+        [Test]
+        public void SubTest()
+        {
+            SimpleArithmetric TestTarget = new SimpleArithmetric(5, 3);
+            Assert.AreEqual(TestTarget.Sub(), 2);
+        }
+    }
+}
