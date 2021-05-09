1444a3ac2ca8f5ba2079565ef351d6cc5f9b2fb4
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-09 20:29:50 +0900

    テスト対象となる簡単なクラスを定義

---
+++ UnitTest/Assets/Scripts/SimpleArithmetric.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// シンプルな演算クラス
+/// </summary>
+public class SimpleArithmetric<T>
+{
+    /// <summary>
+    /// 値１
+    /// </summary>
+    private T ValueA;
+
+    /// <summary>
+    /// 値２
+    /// </summary>
+    private T ValueB;
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    /// <param name="ValueA">値１</param>
+    /// <param name="ValueB">値２</param>
+    public SimpleArithmetric(T ValueA, T ValueB)
+    {
+        this.ValueA = ValueA;
+        this.ValueB = ValueB;
+    }
+
+    /// <summary>
+    /// 加算
+    /// </summary>
+    /// <returns>加算結果</returns>
+    public T Add()
+    {
+        return ValueA + ValueB;
+    }
+
+    /// <summary>
+    /// 減算
+    /// </summary>
+    /// <returns>減算結果</returns>
+    public T Sub()
+    {
+        return ValueA - ValueB;
+    }
+}
